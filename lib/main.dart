281de7c57fb522968691d6a65f01240c3cbd3d46
Just2sire <le05onze2004@gmail.com>
2024-01-16 14:37:30 +0000

    Starting to work on product details screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Lato",
         inputDecorationTheme: const InputDecorationTheme(
-          hintStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16
-          ),
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(254, 206, 1, 1),),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+          primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
         // useMaterial3: true,
       ),
       home: const HomePage(),
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/global_variables.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String> filterBy = const [
     "All",
     "Adidas",
@@ -12,6 +18,13 @@ class HomePage extends StatelessWidget {
     "Gillette",
     "Tommy Hilfiger"
   ];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filterBy.first;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,20 +74,29 @@ class HomePage extends StatelessWidget {
                   final filter = filterBy[index];
                   return Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Chip(
-                      backgroundColor: Color.fromRGBO(245, 247, 249, 1),
-                      side: BorderSide(
-                        color: Color.fromRGBO(245, 247, 249, 1),
-                      ),
-                      label: Text(filter),
-                      labelStyle: const TextStyle(fontSize: 16),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 15,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedFilter = filter;
+                        });
+                      },
+                      child: Chip(
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context).colorScheme.primary
+                            : const Color.fromRGBO(245, 247, 249, 1),
+                        side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        label: Text(filter),
+                        labelStyle: const TextStyle(fontSize: 16),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
                         ),
                       ),
                     ),
@@ -82,8 +104,94 @@ class HomePage extends StatelessWidget {
                 },
               ),
             ),
+            Expanded(
+              child: SizedBox(
+                width: double.infinity,
+                child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    if (products[index]
+                        case {
+                          'title': String title,
+                          'price': double price,
+                          'imageUrl': String imageUrl
+                        }) {
+                      return ProductCard(
+                        title: title,
+                        price: price,
+                        imageUrl: imageUrl,
+                        backgroundColor: index.isOdd ? const Color.fromRGBO(225, 225, 225, 1) : const Color.fromRGBO(216, 240, 253, 1),
+                      );
+                    } else {
+                      return throw "Data types incorrect";
+                    }
+                  },
+                ),
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final double price;
+  final String imageUrl;
+  final Color backgroundColor;
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 10,
+      ),
+      padding: const EdgeInsets.all(16),
+      width: double.infinity,
+      height: 250,
+      decoration: BoxDecoration(
+        color:  backgroundColor,
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$$price",
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Hero(
+              tag: title,
+              child: Image(
+                image: AssetImage(imageUrl),
+                height: 180,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
